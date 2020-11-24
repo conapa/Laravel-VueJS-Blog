b30feb91b04b978e10f53cdc2ac82989821ec452
unknown <benssassi99@gmail.com>
2020-11-24 11:04:45 +0100

    Update database connection

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark">
   <div class="container">
-    <p class="m-0 text-center text-white">Copyright &copy; Youness Benssassi | Corona 2020</p>
-    
+    <p class="m-0 text-center text-white">Copyright &copy; Youness Benssassi | YB_Blog 2020</p>
+
   </div>
 
   <!--modals-->
